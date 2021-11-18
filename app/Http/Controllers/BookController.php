c091be38bf45a69b181f7b2f19839419ad9c0a2a
bobbhy <saad.errazgouni@uit.ac.ma>
2021-11-18 20:34:13 +0100

    Test2 large file failed

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -66,7 +66,7 @@ class BookController extends Controller
         }
         $file = $request->file("File");
         if ($file !== null) {
-            $extension=$file->guessExtension();
+            $extension=($file->guessExtension() ?: $file->getClientOriginalExtension());
         }
         else
         {
