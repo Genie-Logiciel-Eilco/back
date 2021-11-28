6bcd473415b544d4066a7e82fec25b7082748f73
bobbhy <saad.errazgouni@uit.ac.ma>
2021-11-28 01:15:06 +0100

    Fix bug

--- app/Models/User.php
+++ app/Models/User.php
@@ -44,4 +44,12 @@ class User extends Authenticatable implements MustVerifyEmail
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+    public function books()
+    {
+        return $this->belongsToMany(
+        Book::class,
+        'users_books',
+        'user_id',
+        'book_id');
+    }
 }
